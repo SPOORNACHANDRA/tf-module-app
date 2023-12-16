baf29be6aa670d4157e7274d94f2bc240b603015
s poorna chandra <poornanaidu1@gmail.com>
2023-12-16 12:20:58 +0530

    terraform

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_launch_template" "main" {
   user_data = base64encode(templatefile("${path.module}/userdata.sh",
     {
       component = var.component
-      env       = var.env
+
     }))
 
   tag_specifications {
@@ -42,7 +42,7 @@ resource "aws_launch_template" "main" {
 
 
 resource "aws_autoscaling_group" "main" {
-  name                = "${local.name_prefix}-asg"
+  name                = local.name_prefix
   vpc_zone_identifier = var.subnet_ids
   desired_capacity    = var.desired_capacity
   max_size            = var.max_size
