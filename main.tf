638ef1049ad66192e97d6b23d07cf19d91768d95
s poorna chandra <poornanaidu1@gmail.com>
2023-12-16 12:13:07 +0530

    terraform

--- main.tf
+++ main.tf
@@ -5,33 +5,29 @@ resource "aws_security_group" "main" {
   tags        = merge(local.tags, { Name = "${local.name_prefix}-sg" })
 
   ingress {
-    description = "app"
+    description = "APP"
     from_port   = var.port
     to_port     = var.port
     protocol    = "tcp"
     cidr_blocks = var.sg_ingress_cidr
   }
+
   ingress {
-    description = "ssh"
-    from_port   = var.port
-    to_port     = var.port
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = var.ssh_ingress_cidr
   }
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+
 }
 
 resource "aws_launch_template" "main" {
-  name   = local.name_prefix
-  image_id      = data.aws_ami.ami.id
-  instance_type = var.instance_type
+  name                   = local.name_prefix
+  image_id               = data.aws_ami.ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
+  }
 
   user_data = base64encode(templatefile("${path.module}/userdata.sh",
     {
@@ -41,16 +37,17 @@ resource "aws_launch_template" "main" {
 
   tag_specifications {
     resource_type = "instance"
-    tags        = merge(local.tags, { Name = "${local.name_prefix}-ec2" })
+    tags          = merge(local.tags, { Name = "${local.name_prefix}-ec2" })
   }
-}
+
+
 
 resource "aws_autoscaling_group" "main" {
-  name        = "${local.name_prefix}-asg"
+  name                = "${local.name_prefix}-asg"
   vpc_zone_identifier = var.subnet_ids
-  desired_capacity   = var.desired_capacity
-  max_size           = var.max_size
-  min_size           = var.min_size
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
 
   launch_template {
     id      = aws_launch_template.main.id
@@ -58,7 +55,8 @@ resource "aws_autoscaling_group" "main" {
   }
   tag {
     key                 = "Name"
-    propagate_at_launch = true
     value               = local.name_prefix
+    propagate_at_launch = true
   }
 }
+
