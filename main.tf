a48cce37e544640e83e28147f19028eda987b596
s poorna chandra <poornanaidu1@gmail.com>
2023-12-18 15:59:08 +0530

    terraform

--- main.tf
+++ main.tf
@@ -19,6 +19,13 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = var.ssh_ingress_cidr
   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 resource "aws_launch_template" "main" {
--- vars.tf
+++ vars.tf
@@ -4,14 +4,14 @@ variable "subnet_ids" {}
 variable "vpc_id" {}
 variable "component" {}
 variable "port" {}
-variable "ssh_ingress_cidr" {}
-variable "sg_ingress_cidr" {}
-variable "zone_id" {}
-variable "instance_type" {}
-variable "desired_capacity" {}
-variable "max_size" {}
-variable "min_size" {}
-variable "alb_name" {}
-
+#variable "ssh_ingress_cidr" {}
+#variable "sg_ingress_cidr" {}
+#variable "zone_id" {}
+#variable "instance_type" {}
+#variable "desired_capacity" {}
+#variable "max_size" {}
+#variable "min_size" {}
+#variable "alb_name" {}
+#
 
 
