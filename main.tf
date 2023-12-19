9bc70b28b18ead7bbd587ec36c72c0522882a8d3
s poorna chandra <poornanaidu1@gmail.com>
2023-12-19 14:15:11 +0530

    terraform

--- main.tf
+++ main.tf
@@ -95,15 +95,15 @@ resource "aws_lb_listener_rule" "main" {
 
   condition {
     host_header {
-      values = [var.component == "frontend" ? "${var.env}.poorandevops.online":"${var.component}-{var.env}.poornadevops.online"]
+      values = [var.component == "frontend" ? "${var.env}.poorandevops.online" : "${var.component}-{var.env}.poornadevops.online"]
     }
   }
 }
 
-resource "aws_lb_target_group" "public" {
-  count = var.component == "frontend" ? 1:0
-  name     = local.name_prefix
-  port     = var.port
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
-}
+#resource "aws_lb_target_group" "public" {
+#  count = var.component == "frontend" ? 1:0
+#  name     = local.name_prefix
+#  port     = var.port
+#  protocol = "HTTP"
+#  vpc_id   = var.vpc_id
+#}
