4c85d7dd83c503a5db71dbdfb655111e91471d60
s poorna chandra <poornanaidu1@gmail.com>
2023-12-20 16:25:48 +0530

    terraform

--- main.tf
+++ main.tf
@@ -108,14 +108,14 @@ resource "aws_lb_target_group" "public" {
   protocol = "HTTP"
   vpc_id   = var.default_vpc_id
 }
-#
-#resource "aws_lb_target_group_attachment" "public" {
-#  count = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
-#  target_group_arn = aws_lb_target_group.public[0].arn
-#  target_id         = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
-#  port             = 80
-#  availability_zone = "all"
-#}
+
+resource "aws_lb_target_group_attachment" "public" {
+  count = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
+  target_group_arn = aws_lb_target_group.public[0].arn
+  target_id         = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
+  port             = 80
+  availability_zone = "all"
+}
 ###resource "aws_lb_target_group_attachment" "public" {
 ###  count             = var.component == "frontend" ? length(var.az) : 0
 ###  target_group_arn  = aws_lb_target_group.public[0].arn
