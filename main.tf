2c00a325422279083c32d2a2410c8adc0c44a9c4
s poorna chandra <poornanaidu1@gmail.com>
2023-12-20 16:10:49 +0530

    terraform

--- main.tf
+++ main.tf
@@ -116,3 +116,9 @@ resource "aws_lb_target_group_attachment" "public" {
   port             = 80
   availability_zone = "all"
 }
+#resource "aws_lb_target_group_attachment" "public" {
+#  count             = var.component == "frontend" ? length(var.az) : 0
+#  target_group_arn  = aws_lb_target_group.public[0].arn
+#  target_id         = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
+#  port              = 80
+#  availability_zone = "all"
