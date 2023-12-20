73d49c73e39a94cd59b29114acd72aec138c0c58
s poorna chandra <poornanaidu1@gmail.com>
2023-12-20 15:19:41 +0530

    terraform

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "aws_route53_record" "main" {
   name    = var.component == "frontend" ? var.env : "${var.component}-${var.env}"
   type    = "CNAME"
   ttl     = 30
-  records = [var.component == "frontend" ? var.public_alb_name :var.private_alb_name]
+  records = [var.component == "frontend" ? var.public_alb_name : var.private_alb_name]
 }
 
 resource "aws_lb_target_group" "main" {
@@ -100,11 +100,17 @@ resource "aws_lb_listener_rule" "main" {
   }
 }
 
-resource "aws_lb_target_group" "public" {
-  count = var.component == "frontend" ? 1:0
-  name     = "${ local.name_prefix }-public"
-  port     = var.port
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
-}
+#resource "aws_lb_target_group" "public" {
+#  count = var.component == "frontend" ? 1:0
+#  name     = "${ local.name_prefix }-public"
+#  port     = var.port
+#  protocol = "HTTP"
+#  vpc_id   = var.vpc_id
+#}
 
+#resource "aws_lb_target_group_attachment" "public" {
+#
+#  target_group_arn = aws_lb_target_group.public[0].arn
+#  target_id        = aws_instance.main.id
+#  port             = 80
+#}
