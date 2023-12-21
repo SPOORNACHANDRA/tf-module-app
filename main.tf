af035eeca2873da31911a33d6d5f6c78ff1b2032
s poorna chandra <poornanaidu1@gmail.com>
2023-12-21 20:47:46 +0530

    terraform

--- main.tf
+++ main.tf
@@ -65,16 +65,15 @@ resource "aws_autoscaling_group" "main" {
     propagate_at_launch = true
   }
 }
-#
-#
-#resource "aws_route53_record" "main" {
-#  zone_id = var.zone_id
-#  name    = var.component == "frontend" ? var.env : "${var.component}-${var.env}"
-#  type    = "CNAME"
-#  ttl     = 30
-#  records = [var.component == "frontend" ? var.public_alb_name : var.private_alb_name]
-#}
-#
+
+resource "aws_route53_record" "main" {
+  zone_id = var.zone_id
+  name    = var.component == "frontend" ? var.env : "${var.component}-${var.env}"
+  type    = "CNAME"
+  ttl     = 30
+  records = [var.component == "frontend" ? var.public_alb_name : var.private_alb_name]
+}
+
 resource "aws_lb_target_group" "main" {
   name     = local.name_prefix
   port     = var.port
