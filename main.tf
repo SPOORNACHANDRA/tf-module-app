d3d08ab915fa2e0d9fcf5495f12258de31703438
s poorna chandra <poornanaidu1@gmail.com>
2023-12-21 20:54:08 +0530

    terraform

--- main.tf
+++ main.tf
@@ -80,22 +80,22 @@ resource "aws_lb_target_group" "main" {
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 }
-#
-#resource "aws_lb_listener_rule" "main" {
-#  listener_arn = var.private_listener
-#  priority     = var.lb_priority
-#
-#  action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.main.arn
-#  }
-#
-#  condition {
-#    host_header {
-#      values = [var.component == "frontend" ? "${var.env}.poorandevops.online" : "${var.component}-${var.env}.poornadevops.online"]
-#    }
-#  }
-#}
+
+resource "aws_lb_listener_rule" "main" {
+  listener_arn = var.private_listener
+  priority     = var.lb_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header {
+      values = [var.component == "frontend" ? "${var.env}.poorandevops.online" : "${var.component}-${var.env}.poornadevops.online"]
+    }
+  }
+}
 #
 #resource "aws_lb_target_group" "public" {
 #  count       = var.component == "frontend" ? 1 : 0
