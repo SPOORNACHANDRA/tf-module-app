05dd143e486d29dbe20e417d044f5e97ab4d6eb3
s poorna chandra <poornanaidu1@gmail.com>
2023-12-21 20:41:04 +0530

    terraform

--- main.tf
+++ main.tf
@@ -46,26 +46,25 @@ resource "aws_launch_template" "main" {
     tags          = merge(local.tags, { Name = "${local.name_prefix}-ec2" })
   }
 }
-#
-#
-#resource "aws_autoscaling_group" "main" {
-#  name                = "${local.name_prefix}-asg"
-#  vpc_zone_identifier = var.subnet_ids
-#  desired_capacity    = var.desired_capacity
-#  max_size            = var.max_size
-#  min_size            = var.min_size
-#  target_group_arns   = [aws_lb_target_group.main.arn]
-#
-#  launch_template {
-#    id      = aws_launch_template.main.id
-#    version = "$Latest"
-#  }
-#  tag {
-#    key                 = "Name"
-#    value               = local.name_prefix
-#    propagate_at_launch = true
-#  }
-#}
+
+resource "aws_autoscaling_group" "main" {
+  name                = "${local.name_prefix}-asg"
+  vpc_zone_identifier = var.subnet_ids
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
+  target_group_arns   = [aws_lb_target_group.main.arn]
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
+  tag {
+    key                 = "Name"
+    value               = local.name_prefix
+    propagate_at_launch = true
+  }
+}
 #
 #
 #resource "aws_route53_record" "main" {
@@ -76,12 +75,12 @@ resource "aws_launch_template" "main" {
 #  records = [var.component == "frontend" ? var.public_alb_name : var.private_alb_name]
 #}
 #
-#resource "aws_lb_target_group" "main" {
-#  name     = local.name_prefix
-#  port     = var.port
-#  protocol = "HTTP"
-#  vpc_id   = var.vpc_id
-#}
+resource "aws_lb_target_group" "main" {
+  name     = local.name_prefix
+  port     = var.port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
 #
 #resource "aws_lb_listener_rule" "main" {
 #  listener_arn = var.private_listener
