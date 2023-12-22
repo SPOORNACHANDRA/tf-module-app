c42cb5a2e682a0feb120dabbb7d3695b7c29145b
s poorna chandra <poornanaidu1@gmail.com>
2023-12-22 20:28:56 +0530

    terraform

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 yum install ansible -y &>>/opt/userdata.log
-yum install python3.11.-pip.norch-y &>>/opt/userdata.log
-pip3.11 install botocore boto3 &>>/opt/userdata.log
+#yum install python3.11.-pip.norch-y &>>/opt/userdata.log
+#pip3.11 install botocore boto3 &>>/opt/userdata.log
 ansible-pull -i localhost, -U https://github.com/SPOORNACHANDRA/ansible-roboshop-v1.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
