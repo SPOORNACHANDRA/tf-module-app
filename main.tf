0dcdcb48e13abfefb1734068006cc20a41ea74ad
s poorna chandra <poornanaidu1@gmail.com>
2023-12-22 18:15:19 +0530

    terraform

--- main.tf
+++ main.tf
@@ -107,7 +107,7 @@ resource "aws_lb_target_group" "public" {
 }
 
 resource "aws_lb_target_group_attachment" "public" {
-  count             = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
+  count             = var.component == "frontend" ? length(var.subnet_ids) : 0
   target_group_arn  = aws_lb_target_group.public[0].arn
   target_id         = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
   port              = 80
@@ -131,3 +131,54 @@ resource "aws_lb_listener_rule" "public" {
   }
 }
 
+#resource "aws_iam_policy" "main" {
+#  name        = "${local.name_prefix}-policy"
+#  path        = "/"
+#  description = "${local.name_prefix}-policy"
+#
+#  policy = jsonencode({
+#    "Version": "2012-10-17",
+#    "Statement": [
+#      {
+#        "Sid": "VisualEditor0",
+#        "Effect": "Allow",
+#        "Action": [
+#          "ssm:GetParameterHistory",
+#          "ssm:GetParametersByPath",
+#          "ssm:GetParameter"
+#        ],
+#        "Resource": "arn:aws:ssm:us-east-1:931263538628:parameter/${var.env}.*"
+#      },
+#      {
+#        "Sid": "VisualEditor1",
+#        "Effect": "Allow",
+#        "Action": "ssm:DescribeParameters",
+#        "Resource": "*"
+#      }
+#    ]
+#  })
+#}
+#
+#
+#resource "aws_iam_role" "main" {
+#  name = "${local.name_prefix}-role"
+#
+#  # Terraform's "jsonencode" function converts a
+#  # Terraform expression result to valid JSON syntax.
+#  assume_role_policy = jsonencode({
+#    Version   = "2012-10-17"
+#    Statement = [
+#      {
+#        Action    = "sts:AssumeRole"
+#        Effect    = "Allow"
+#        Sid       = ""
+#        Principal = {
+#          Service = "ec2.amazonaws.com"
+#        }
+#      },
+#    ]
+#  })
+#
+#  tags = merge(local.tags, { Name = "${local.name_prefix}-role" })
+#
+#}
