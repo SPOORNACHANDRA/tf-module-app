3032f70506afdb0da47bb3eec0d1be1ca4d4a2d4
s poorna chandra <poornanaidu1@gmail.com>
2023-12-26 18:36:08 +0530

    terraform

--- main.tf
+++ main.tf
@@ -33,9 +33,9 @@ resource "aws_launch_template" "main" {
   image_id               = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
-#  iam_instance_profile {
-#    name = "${local.name_prefix}-role"
-#  }
+  iam_instance_profile {
+    name = "${local.name_prefix}-role"
+  }
 
   user_data = base64encode(templatefile("${path.module}/userdata.sh",
     {
@@ -185,8 +185,8 @@ resource "aws_iam_role_policy_attachment" "attach" {
   role       = aws_iam_role.main.name
   policy_arn = aws_iam_policy.main.arn
 }
-#
-#resource "aws_iam_instance_profile" "main" {
-#  name = "${local.name_prefix}-role"
-#  role = aws_iam_role.main.name
-#}
+
+resource "aws_iam_instance_profile" "main" {
+  name = "${local.name_prefix}-role"
+  role = aws_iam_role.main.name
+}
