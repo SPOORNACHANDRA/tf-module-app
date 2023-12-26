9c8999973bde649dc251299dd640f6ddc53b3e14
s poorna chandra <poornanaidu1@gmail.com>
2023-12-26 10:56:51 +0530

    terraform

--- main.tf
+++ main.tf
@@ -140,23 +140,25 @@ resource "aws_iam_policy" "main" {
   description = "${local.name_prefix}-policy"
 
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Sid": "VisualEditor0",
-        "Effect": "Allow",
-        "Action": [
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "kms:Decrypt",
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
+          "ssm:GetParameters",
           "ssm:GetParameter"
         ],
         "Resource": "arn:aws:ssm:us-east-1:931263538628:parameter/docdb.${var.env}.*"
       },
       {
-        "Sid": "VisualEditor1",
-        "Effect": "Allow",
-        "Action": "ssm:DescribeParameters",
-        "Resource": "*"
+        "Sid" : "VisualEditor1",
+        "Effect" : "Allow",
+        "Action" : "ssm:DescribeParameters",
+        "Resource" : "*"
       }
     ]
   })
