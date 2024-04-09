247480e63f5435ddd2aa1d257ac7b43f92ad86b7
s poorna chandra <poornanaidu1@gmail.com>
2024-04-09 15:42:39 +0530

    changes

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = var.ssh_ingress_cidr
   }
+
   ingress {
     description = "PROMETHEUS"
     from_port   = 9100
@@ -26,6 +27,7 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = var.monitoring_ingress_cidr
   }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -36,14 +38,14 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_security_group_rule" "nginx_exporter" {
-  count = var.component == "frontend" ? 1:0
+  count             = var.component == "frontend" ? 1 : 0
   type              = "ingress"
   from_port         = 9113
   to_port           = 9113
   protocol          = "tcp"
   cidr_blocks       = var.monitoring_ingress_cidr
   security_group_id = aws_security_group.main.id
-  description = "Nginx Prometheus Exporter"
+  description       = "Nginx Prometheus Exporter"
 }
 
 resource "aws_iam_policy" "main" {
@@ -52,28 +54,48 @@ resource "aws_iam_policy" "main" {
   description = "${local.name_prefix}-policy"
 
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Sid": "VisualEditor0",
-        "Effect": "Allow",
-        "Action": [
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
           "kms:Decrypt",
           "ssm:GetParameterHistory",
           "ssm:GetParametersByPath",
           "ssm:GetParameters",
           "ssm:GetParameter"
         ],
-        "Resource": local.policy_resources
+        "Resource" : local.policy_resources
       },
       {
-        "Sid": "VisualEditor1",
-        "Effect": "Allow",
-        "Action": "ssm:DescribeParameters",
-        "Resource": "*"
+        "Sid" : "VisualEditor1",
+        "Effect" : "Allow",
+        "Action" : "ssm:DescribeParameters",
+        "Resource" : "*"
       }
     ]
   })
+}
+
+resource "aws_iam_role" "main" {
+  name = "${local.name_prefix}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(local.tags, { Name = "${local.name_prefix}-role" })
 }
 
 resource "aws_iam_role_policy_attachment" "attach" {
@@ -101,23 +123,23 @@ resource "aws_launch_template" "main" {
       env       = var.env
     }))
 
-
-#    block_device_mappings {
-#      device_name = "/dev/sda1"
-#
-#      ebs {
-#        delete_on_termination = "true"
-#        encrypted             = "true"
-#        kms_key_id            = var.kms_key_id
-#        volume_size           = 10
-#        volume_type           = "gp2"
-#      }
-#    }
+  #  block_device_mappings {
+  #    device_name = "/dev/sda1"
+  #
+  #    ebs {
+  #      delete_on_termination = "true"
+  #      encrypted             = "true"
+  #      kms_key_id            = var.kms_key_id
+  #      volume_size           = 10
+  #      volume_type           = "gp2"
+  #    }
+  #  }
 
   tag_specifications {
     resource_type = "instance"
     tags          = merge(local.tags, { Name = "${local.name_prefix}-ec2" })
   }
+
 }
 
 resource "aws_autoscaling_group" "main" {
@@ -159,7 +181,7 @@ resource "aws_autoscaling_policy" "asg-cpu-rule" {
 
 resource "aws_route53_record" "main" {
   zone_id = var.zone_id
-  name    = var.component == "frontend" ? var.env : "${var.component}-${var.env}"
+  name    = var.component == "frontend" ? var.env == "prod" ? "www" : var.env : "${var.component}-${var.env}"
   type    = "CNAME"
   ttl     = 30
   records = [var.component == "frontend" ? var.public_alb_name : var.private_alb_name]
@@ -170,7 +192,7 @@ resource "aws_lb_target_group" "main" {
   port     = var.port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
-
+  deregistration_delay = 15
   health_check {
     enabled             = true
     healthy_threshold   = 2
@@ -194,7 +216,7 @@ resource "aws_lb_listener_rule" "main" {
 
   condition {
     host_header {
-      values = [var.component == "frontend" ? "${var.env}.poornadevops.online" : "${var.component}-${var.env}.poornadevops.online"]
+      values = [var.component == "frontend" ? "${var.env == "prod" ? "www" : var.env}.poornadevops.online" : "${var.component}-${var.env}.poornadevops.online"]
     }
   }
 }
@@ -217,16 +239,17 @@ resource "aws_lb_target_group" "public" {
     unhealthy_threshold = 2
     matcher             = "404"
   }
+
 }
 
 resource "aws_lb_target_group_attachment" "public" {
-  count             = var.component == "frontend" ? length(var.subnet_ids) : 0
-  #count             = var.component == "frontend" ? length(var.subnet_ids) : 0
+  count             = var.component == "frontend" ? length(var.az) : 0
   target_group_arn  = aws_lb_target_group.public[0].arn
   target_id         = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index)
   port              = 80
   availability_zone = "all"
 }
+
 
 resource "aws_lb_listener_rule" "public" {
   count        = var.component == "frontend" ? 1 : 0
@@ -240,28 +263,7 @@ resource "aws_lb_listener_rule" "public" {
 
   condition {
     host_header {
-      values = ["${var.env}.poornadevops.online"]
+      values = ["${var.env == "prod" ? "www" : var.env}.poornadevops.online"]
     }
   }
-}
-
-
-
-resource "aws_iam_role" "main" {
-  name = "${local.name_prefix}-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-  tags          = merge(local.tags, { Name = "${local.name_prefix}-role" })
 }
